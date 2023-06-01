b83155c7ee7e49e4164b198ea2062dfddc530877
Caryl Wyatt <carylw@hathitrust.org>
2023-06-01 13:39:32 -0500

    add environment variable for loading min files

--- functions.php
+++ functions.php
@@ -25,15 +25,30 @@
 	function pg_enqueue_site_files() {
 
 		wp_enqueue_style( 'site-fonts', get_template_directory_uri() . '/fonts.min.css', NULL, filemtime( get_template_directory() . '/fonts.css' ) );
-		wp_enqueue_style( 'firebird-styles', 'https://hathitrust-firebird-common.netlify.app/assets/main.css');
-		wp_enqueue_style( 'site-styles', get_template_directory_uri() . '/style.min.css', array( 'site-fonts' ), filemtime( get_template_directory() . '/style.min.css' ) );
+		if ( 'local' === wp_get_environment_type() ) {
+			wp_enqueue_style( 'firebird-styles', 'https://hathitrust-firebird-common.netlify.app/assets/main.css');
+			wp_enqueue_style( 'site-styles', get_template_directory_uri() . '/style.css', array( 'site-fonts' ), filemtime( get_template_directory() . '/style.min.css' ) );
+		} else {
+			//need min version of firebird
+			wp_enqueue_style( 'firebird-styles', 'https://hathitrust-firebird-common.netlify.app/assets/main.css');
+			wp_enqueue_style( 'site-styles', get_template_directory_uri() . '/style.min.css', array( 'site-fonts' ), filemtime( get_template_directory() . '/style.min.css' ) );
+		}	
 
-		if ( is_front_page() ) {
+
+		if ( is_front_page() && 'local' === wp_get_environment_type() ) {
+			wp_enqueue_style( 'home-styles', get_template_directory_uri() . '/home.css', array( 'site-fonts', 'site-styles' ), filemtime( get_template_directory() . '/home.min.css' ) );
+		} else if ( is_front_page() ) {
 			wp_enqueue_style( 'home-styles', get_template_directory_uri() . '/home.min.css', array( 'site-fonts', 'site-styles' ), filemtime( get_template_directory() . '/home.min.css' ) );
 		}
 
-		wp_enqueue_script( 'firebird-scripts', 'https://hathitrust-firebird-common.netlify.app/assets/main.js', array(), false, false);
-		wp_enqueue_script( 'site-scripts', get_template_directory_uri() . '/js/script.js', array('firebird-scripts'), filemtime( get_template_directory() . '/js/scripts.min.js' ), TRUE );
+		if ( 'local' === wp_get_environment_type() ) {
+			wp_enqueue_script( 'firebird-scripts', 'https://hathitrust-firebird-common.netlify.app/assets/main.js', array(), false, false);
+			wp_enqueue_script( 'site-scripts', get_template_directory_uri() . '/js/scripts.js', array('firebird-scripts'), filemtime( get_template_directory() . '/js/scripts.js' ), TRUE );
+		} else {
+			//need min version of firebird
+			wp_enqueue_script( 'firebird-scripts', 'https://hathitrust-firebird-common.netlify.app/assets/main.js', array(), false, false);
+			wp_enqueue_script( 'site-scripts', get_template_directory_uri() . '/js/scripts.min.js', array('firebird-scripts'), filemtime( get_template_directory() . '/js/scripts.min.js' ), TRUE );
+		}
 		wp_enqueue_script( 'fontawesome-scripts', 'https://kit.fontawesome.com/1c6c3b2b35.js');
 		wp_enqueue_script( 'matomo-script', get_template_directory_uri() . '/js/matomo.js');
 
