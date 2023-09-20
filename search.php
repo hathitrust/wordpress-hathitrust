4a084dfe289d2a61210faabcaa5f92ab28a6a0e2
Caryl Wyatt <carylw@hathitrust.org>
2023-09-20 10:35:58 -0500

    strip tags from search excerpt

--- search.php
+++ search.php
@@ -58,7 +58,8 @@
 						
 						if (get_row_layout() == 'content' && $i == 0) {
 							$i++;	
-								$text = wp_kses_post( get_sub_field( 'content', false) ); 	
+								$text = wp_kses_post( get_sub_field( 'content') ); 	
+								$text = strip_tags($text);
 								if (strlen($text) > 250) {
 									$truncated_text = substr($text, 0, 250);
 									$last_space = strrpos($truncated_text, " ");
