e8a001b15e9156f962a83f82bd55858d83555da9
Caryl Wyatt <carylw@hathitrust.org>
2025-09-30 14:08:58 -0400

    fix feedback modal button

--- inc/block-feedback_form.php
+++ inc/block-feedback_form.php
@@ -2,10 +2,9 @@
 	<h2 id="<?= sanitize_title_with_dashes( $heading ); ?>" class="pb-heading allcaps"><?= esc_html( get_sub_field( 'heading' ) ); ?></h2>
 	<div class="pb">
 		<div class="pb-contents"><?= wp_kses_post( get_sub_field( 'contents' ) ); ?></div>
-		<div class="pb-link" data-hathi-trigger="hathi-feedback-form-modal" id="feedback-form">
+		<div class="pb-link" data-hathi-trigger="hathi-feedback-form-modal" data-prop-form="contact" data-prop-is-open="false" id="feedback-form" >
 			<button type="button" class="btn btn-primary"><span><?= get_sub_field( 'label' ); ?></span></button>
 		</div>
 	</div>
-	<hathi-feedback-form-modal data-prop-form="basic" data-prop-is-open="false"></hathi-feedback-form-modal">
 </div>
 
